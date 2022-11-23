ccfc226a3c5f85c1a3cd10e0f0614e2530e664a3
CristianLRubio <cleonardor@correo.udistrital.edu.co>
2022-11-23 16:56:41 -0500

    v 0.0.1

---
+++ Punto.py
@@ -0,0 +1,13 @@
+import math
+class Punto:
+    def __init__(self):
+        self.x=int
+        self.y=int
+    
+    def calcular_distancia(self):
+        self.calcular_distancia=math.hypot(self.x,self.y)
+
+
+
+miPunto=Punto()
+otroPunto=Punto()
